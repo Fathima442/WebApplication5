6b5bb7789d67ba3880e186a2e2948311a8eed3f7
Fathima442 <117431123+Fathima442@users.noreply.github.com>
2022-11-17 10:08:20 +0530

    Add project files.

---
+++ WebApplication5/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace WebApplication5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<List<UserProfile>>> GetAllUserProfiles()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<UserProfile> users = await SelectAllUsers(connection);
+            return Ok(users);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<UserProfile>> GetUser(int userId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync<UserProfile>("select * from userprofile where id=@Id", new {Id=userId});
+            return Ok(user);
+        }
+        private static async Task<IEnumerable<UserProfile>> SelectAllUsers(SqlConnection connection)
+        {
+            return await connection.QueryAsync<UserProfile>("select * from userprofile");
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<UserProfile>>> CreateUser(UserProfile user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into userprofile(firstName,lastName,email,passcode) values(@FirstName,@LastName,@Email,@Passcode)",user);
+            return Ok(await SelectAllUsers(connection));
+            
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<UserProfile>>> UpdateUser(UserProfile user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update userprofile set firstName=@FirstName,lastName=@LastName,email=@email,passcode=@passcode where id=@Id", user);
+            return Ok(await SelectAllUsers(connection));
+
+        }
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult<List<UserProfile>>> DeleteUser(int userId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from userprofile where id=@Id", new {Id=userId});
+            return Ok(await SelectAllUsers(connection));
+
+        }
+    }
+}
---
+++ WebApplication5/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication5
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }=String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string Passcode { get; set; } = String.Empty;
+
+
+
+
+    }
+}
